1fd5ce7e58cd228dc94b21b591230dda86faee69
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-03-30 13:25:57 +0200

    chore: added ncu and modified husky hooks for that

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged',
+    'pre-commit': 'npm run check',
     'pre-push': 'npm test',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
