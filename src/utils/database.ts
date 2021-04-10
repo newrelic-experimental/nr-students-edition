ea1bbe14a06449dc7c5033276cdc388c27cd7bb4
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-04-10 08:52:19 +0200

    feat: added db connection and types

---
+++ src/utils/database.ts
@@ -0,0 +1,18 @@
+import { DatabaseContext } from '../types/database';
+import { config } from '../config';
+import DataApiClient from 'data-api-client';
+
+const databaseContext: DatabaseContext = {
+  resourceArn: config.DATABASE_RESOURCE_ARN,
+  secretArn: config.DATABASE_SECRET_ARN,
+  database: config.DATABASE,
+};
+
+const dbClient = DataApiClient(databaseContext);
+
+
+// TODO: setup the return type based on database model
+export const getAll = async (): Promise<any | undefined> => {
+  const result = await dbClient.query(`SELECT * FROM validation_attempts'`);
+  return result;
+};
