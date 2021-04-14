745e661ec57e90e3e9c09537bdffcdcafb6ab82a
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-04-14 07:51:43 +0200

    feat: defined lambdas based on serverless

---
+++ src/functions/check-validation-status.ts
@@ -0,0 +1,28 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { LambdaResponse } from '../types/response';
+import { Logger } from '../utils/logger';
+import { Context } from 'aws-lambda/handler';
+import { badRequestError } from '../utils/errors';
+
+
+export const check = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
+  const logger = new Logger(context);
+  const params = event.queryStringParameters || {};
+  let accountId: string;
+
+  if (params.accountId) {
+    accountId = params.accountId;
+  } else {
+    return badRequestError;
+  }
+
+  logger.info(`Getting validation status for ${accountId}`);
+
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    statusCode: 200,
+    body: 'For now you cannot check your status hehe',
+  };
+};
