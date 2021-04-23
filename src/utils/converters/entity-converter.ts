f46f569c536b927df8b2da0d4e8729a2ec992cb6
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-04-23 16:39:24 +0200

    feat: added converters for entity and dto

---
+++ src/utils/converters/entity-converter.ts
@@ -0,0 +1,20 @@
+import { StudentEntity, StudentDTO } from '../../types/person';
+
+export const convertEntityToDTO = (entity: StudentEntity): StudentDTO => {
+  const dto: StudentDTO = {
+    accountId: entity.account_id,
+    nrEmail: entity.nr_email,
+    userEmail: entity.user_email,
+    firstname: entity.name,
+    lastname: entity.surname,
+    university: entity.university,
+    levelOfStudy: entity.level_of_study,
+    graduationDate: entity.graduation_date,
+    country: entity.country,
+    isThirteenYo: entity.is_thirteen_yo,
+    parentsEmail: entity.parents_email,
+    validationStatus: entity.validation_status
+  };
+
+  return dto;
+};
