b9e7268d3e2050429f4124151a2d10ec77842246
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-05-04 13:08:10 +0200

    feat: auth lambda

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -2,19 +2,52 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { Context } from 'aws-lambda/handler';
 import { LambdaResponse } from "../types/response";
 import { Logger } from "../utils/logger";
+import { authDtoSchema, StateEntity } from '../types/state';
+import { ValidationError } from "myzod";
+import { badRequestError, internalLambdaError } from "../utils/errors";
+import { generateState } from "../utils/generators/state-generator";
+import { saveState } from '../utils/database/database';
+import { config } from '../config';
+
+const STATE_SIZE = 32;
+
 
 export const authGithub = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
   const logger = new Logger(context);
   logger.info('Authentication with Github');
 
-  const fakeUrl = 'this is not url';
+  const body = JSON.parse(event.body);
+  const state = generateState(STATE_SIZE);
+  let location = '';
+
+  try {
+    const id = authDtoSchema.parse(body);
+    const stateEntity: StateEntity = {
+      account_id: id.accountId,
+      state: state
+    };
+
+    await saveState(stateEntity);
+
+    location = config.GITHUB_URL.concat(state);
+
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      logger.error(`Validation error: ${error.message}`);
+
+      return badRequestError;
+    }
+
+    logger.error(`Something went wrong: ${error.message}`);
+    return internalLambdaError;
+  }
 
   return {
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Location': fakeUrl
+      'Access-Control-Allow-Origin': 'newrelic.com',
+      'Location': ''
     },
     statusCode: 302,
-    body: ''
+    body: location
   };
 };
