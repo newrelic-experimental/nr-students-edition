e25bad3dc454875d15d71bed097c5c87ca59dc0e
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-05-04 20:45:26 +0200

    feat: added http utils for request github api

---
+++ src/types/github.ts
@@ -0,0 +1,6 @@
+export type GithubCredentials = {
+  clientId: string;
+  clientSecret: string;
+  accessToken: string;
+};
+
---
+++ src/utils/http/request.ts
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import { GithubCredentials } from '../../types/github';
+
+export const sendGetRequest = async <T>(url: string, token: string): Promise<T> => {
+  try {
+    const response = await fetch(url, {
+      method: 'get',
+      headers: {
+        'Authorization': token
+      }
+    });
+
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+
+
+export const sendPostRequest = async <T>(url: string, body: GithubCredentials): Promise<T> => {
+  try {
+    const response = await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(body)
+    });
+
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
