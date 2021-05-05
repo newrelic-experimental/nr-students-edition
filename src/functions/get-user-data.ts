e99972ad96bab51734f063cb33200829f3cc049c
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-05-05 07:39:48 +0200

    feat: checking if state exists in the lambda

--- src/functions/get-user-data.ts
+++ src/functions/get-user-data.ts
@@ -2,9 +2,26 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { LambdaResponse } from '../types/response';
 import { Logger } from '../utils/logger';
 import { Context } from 'aws-lambda/handler';
+import { StateAndCode } from '../types/github';
+import { badRequestError } from '../utils/errors';
+import { checkIfStateExists } from '../utils/database/database';
 
 export const getUserData = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
   const logger = new Logger(context);
+  logger.info(`Getting user data from Github`);
+
+  const params = event.queryStringParameters || {};
+  let stateAndCode: StateAndCode;
+
+  if (params.state && params.code) {
+    stateAndCode.state = params.state;
+    stateAndCode.code = params.code;
+  } else {
+    return badRequestError;
+  }
+
+  const isStateExists = checkIfStateExists(stateAndCode.state);
+  logger.info(`Is state exists: ${isStateExists}`);
 
   return {
     headers: {
--- src/types/github.ts
+++ src/types/github.ts
@@ -1,6 +1,15 @@
+import myzod, { Infer } from 'myzod';
+
+
 export type GithubCredentials = {
   clientId: string;
   clientSecret: string;
   accessToken: string;
 };
 
+export const stateAndCodeSchema = myzod.object({
+  state: myzod.string(),
+  code: myzod.string()
+}).collectErrors();
+
+export type StateAndCode = Infer<typeof stateAndCodeSchema>;
