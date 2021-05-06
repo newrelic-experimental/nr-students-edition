fea023441c41e4fc6bae2a48cb39c0a8980ef7be
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-05-06 15:23:53 +0200

    feat: improved get user data from github flow

--- src/functions/get-user-data.ts
+++ src/functions/get-user-data.ts
@@ -2,12 +2,13 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { LambdaResponse } from '../types/response';
 import { Logger } from '../utils/logger';
 import { Context } from 'aws-lambda/handler';
-import { StateAndCode } from '../types/github';
+import { GithubCredentials, StateAndCode, StudentResponse, StudentResponseGithub, TokenEntity } from '../types/github';
 import { badRequestError, recordNotFound } from '../utils/errors';
-import { getDataFromState } from '../utils/database/database';
+import { getDataFromState, saveAccessToken } from '../utils/database/database';
 import { State } from '../types/database';
-import { sendGetRequest, sendPostRequest } from '../utils/http/request';
+import { sendGetRequest } from '../utils/http/request';
 import { config } from '../config';
+import axios, { AxiosResponse } from 'axios';
 
 export const getUserData = async (event: APIGatewayProxyEvent, context: Context): Promise<LambdaResponse> => {
   const logger = new Logger(context);
@@ -24,31 +25,51 @@ export const getUserData = async (event: APIGatewayProxyEvent, context: Context)
   }
 
   const stateFromDB = await getDataFromState(stateAndCode.state) as State;
-  if (stateFromDB !== undefined) {
+  if (stateFromDB === undefined) {
     return recordNotFound;
   }
 
-  logger.info(`Get data from state: account_id: (${stateFromDB.records[0].account_id}), state: (${stateFromDB.records[0].state})`);
-
   const url = 'https://github.com/login/oauth/access_token';
-  const accessToken = await sendPostRequest(url, {
-    clientId: config.GITHUB_CLIENT_ID,
-    clientSecret: config.GITHUB_SECRET,
+  const body = {
+    client_id: config.GITHUB_CLIENT_ID,
+    client_secret: config.GITHUB_SECRET,
     code: stateAndCode.code
+  } as GithubCredentials;
+
+  const data: AxiosResponse<{access_token: string}> = await axios(url, {
+    headers: {
+      'Accept': 'application/json'
+    },
+    data: body,
+    method: 'POST'
   });
 
-  logger.info(`Access Token: ${JSON.stringify(accessToken)}`);
+  logger.info(`Obtained access token...`);
 
   const userDataUrl = 'https://education.github.com/api/user';
-  const userData = await sendGetRequest(userDataUrl, accessToken as string);
+  const userData = await sendGetRequest(userDataUrl, data.data.access_token) as StudentResponseGithub;
 
-  logger.info(`User data: ${JSON.stringify(userData)}`);
+  logger.info(`Is a student: ${JSON.stringify(userData)}`);
+
+  if (userData.student) {
+    await saveAccessToken(
+      {
+        account_id: stateFromDB.records[0].account_id,
+        access_token: data.data.access_token
+      } as TokenEntity
+    );
+  }
+
+  const response: StudentResponse = {
+    account_id: stateFromDB.records[0].account_id,
+    student: userData.student
+  };
 
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
     statusCode: 200,
-    body: ''
+    body: JSON.stringify(response)
   };
 };
