d9a5908e77e21bc53410cd0ce8cecbb4c3451f45
DominikMarciniszyn <dominik.marciniszyn@codetain.co>
2021-08-17 06:35:40 +0200

    feat: added script to extract domains from jet brains repo

--- scripts/domain-extractor.py
+++ scripts/domain-extractor.py
@@ -1,20 +1,31 @@
+from dataclasses import replace
 from genericpath import isdir
 import os
 import sys
-import pathlib
+from typing import List
+from xml import dom
 
 PATH = "/Users/dmar/New-Relic/jetbrains-swot/lib/domains"
+PREFIX = "/Users/dmar/New-Relic/jetbrains-swot/lib/domains/"
+PATH_TO_SAVE_DOMAINS = ""
+list_of_domains: List = []
 
-
-# print([name for name in os.listdir(PATH) if os.path.isdir(name)])
-# print([os.path.abspath(name) for name in os.listdir(PATH) if os.path.isdir(name)])
-
-# domains = sorted(os.listdir(PATH))
-
-# for path, subdirs, files in os.walk(PATH):
-#     for name in files:
-#         print(pathlib.PurePath(path, name).name.removesuffix(".txt"))
 
 for root, subdirs, files in os.walk(PATH):
-    pass
+    current_path = root.removeprefix(PREFIX)
+    current_path = current_path.split("/")
+    reversed_current_path = current_path[::-1]
+    result = " ".join([str(x) for x in reversed_current_path])
 
+    list_of_domains.append(result)
+
+    print(f"Current Path: {reversed_current_path}")
+
+
+for domain in list_of_domains:
+    domain = domain.replace(" ", ".")
+    print(domain)
+
+    with open('./domains', 'a') as file:
+        if '.' in domain:
+            file.write(domain + '\n')
